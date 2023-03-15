3db8584c8c7311d2ecd652b7420ecc42e9f8eaf5
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-03-15 20:01:04 +0000

    generate new availability data optional

--- Data.py
+++ Data.py
@@ -3,7 +3,7 @@ import random
 
 from pymzn import dzn
 
-##CONSTANTS##
+##PARAMETERS##
 NUM_SUPERVISORS = 20
 NUM_STUDENTS = 100
 
@@ -12,15 +12,16 @@ SESSIONS = 3
 
 MAX_STUDENTS_PER_SESSION = 6
 MAX_STUDENTS_PER_SUPERVISOR = 7
-MAX_SUPERVISORS_PER_SESSION = 6
+MAX_SUPERVISORS_PER_SESSION = 4
 MIN_SUPERVISORS_PER_SESSION = 2
+
+NEW_DATA_FILE = True
 #############
 CSV_FILE = "./data/availability.csv"
 
 
 class Data:
     def generate_availability():
-
         with open(CSV_FILE, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             
@@ -29,18 +30,21 @@ class Data:
                 row = [random.choices(['true', 'false'], weights=(0.7, 0.3))[0] for j in range(TIMESLOTS)]
                 writer.writerow(row)
 
+        
+    def get_availability_data():
+        # Generate new availability lists
+        if(NEW_DATA_FILE):
+            Data.generate_availability()
+
         # Return availability data 
         with open(CSV_FILE) as file_name:
             file_read = csv.reader(file_name)
-
             array = list(file_read)
-        
         return array
-
 
     def generate_data_file(dataFile):
         # Get Supervisor Availability Data
-        availability = Data.generate_availability()
+        availability = Data.get_availability_data()
 
         # Load all data
         data = {
