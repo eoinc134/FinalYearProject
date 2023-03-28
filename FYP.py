25e54fa35491a03935e5cc4708d26db8b701e5cb
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-03-28 18:36:48 +0100

    runtime tests

--- FYP.py
+++ FYP.py
@@ -1,27 +1,41 @@
 from minizinc import Instance, Model, Solver
 from Data import Data
+import time
 
 MODEL_FILE = './models/fyp.mzn'
 DATA_FILE = './data/fyp.dzn'
 SOLVER = 'gecode'
+NUM_RUNS = 5
 
 class FYP():
     def main():
-        ## BUILD MODEL ##
-        fyp = Model(MODEL_FILE)
-
-        Data.generate_data_file(DATA_FILE)
-        fyp.add_file(DATA_FILE)
-
+        total_time = 0
+        succesful_runs = 0
         
-        ## SOLVE INSTANCE ##
-        gecode = Solver.lookup(SOLVER)
-        instance = Instance(gecode, fyp)
-        result = instance.solve()
+        for x in range(NUM_RUNS):
+            start_time = time.time()
+            ## BUILD MODEL ##
+            fyp = Model(MODEL_FILE)
 
-        ## VERIFY OUTPUT ##
-        if(Data.verify_output(result)):
-            print(result)
+            Data.generate_data_file(DATA_FILE)
+            fyp.add_file(DATA_FILE)
+
+            
+            ## SOLVE INSTANCE ##
+            gecode = Solver.lookup(SOLVER)
+            instance = Instance(gecode, fyp)
+            result = instance.solve()
+
+            ## VERIFY OUTPUT ##
+            if(Data.verify_output(result)):
+                #print(result)
+                runtime = round(time.time() - start_time, 3)
+                print(f"Time: {runtime}s")
+                total_time += runtime
+                succesful_runs += 1
+        
+        print(f"Avg Time: {round(total_time / succesful_runs, 3)}s")
+
             
 
     if __name__ == "__main__":
