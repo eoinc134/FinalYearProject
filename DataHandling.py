92927c7ef574fa6ed90e61dc762865b07b76bf68
eoinc134 <60822259+eoinc134@users.noreply.github.com>
2023-04-15 20:15:15 +0100

    code refactored, docstrings added, some code encapsulated to functions

--- DataHandling.py
+++ DataHandling.py
@@ -1,51 +1,56 @@
 import csv
 import random
-
 from pymzn import dzn
 
 ##PARAMETERS##
 NUM_SUPERVISORS = 20
 NUM_STUDENTS = 100
-
 TIMESLOTS = 8
 SESSIONS = 3
-
 MAX_STUDENTS_PER_SESSION = 6
 MAX_STUDENTS_PER_SUPERVISOR = 7
 MAX_SUPERVISORS_PER_SESSION = 4
 MIN_SUPERVISORS_PER_SESSION = 2
-
 availability = 0.7
-
 NEW_DATA_FILE = True
 CSV_FILE = "./data/availability.csv"
-#############
-
-
 
 class Data:
+    """
+    Class to generate and retrieve data for scheduling problem.
+    """
+
+    @staticmethod
     def generate_availability():
+        """
+        Generate availability data for supervisors.
+        """
         with open(CSV_FILE, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            
-            # Write X rows with randomly assigned attributes (70% chance of being True)
-            for i in range(1, NUM_SUPERVISORS+1):
+            for i in range(1, NUM_SUPERVISORS + 1):
                 row = [random.choices(['true', 'false'], weights=(availability, 1- availability))[0] for j in range(TIMESLOTS)]
                 writer.writerow(row)
 
-        
+    @staticmethod
     def get_availability_data():
+        """
+        Retrieve availability data for supervisors.
+        """
         # Generate new availability lists
-        if(NEW_DATA_FILE):
+        if NEW_DATA_FILE:
             Data.generate_availability()
 
-        # Return availability data 
+        # Return availability data
         with open(CSV_FILE) as file_name:
             file_read = csv.reader(file_name)
             array = list(file_read)
         return array
 
-    def generate_data_file(dataFile):
+    @staticmethod
+    def generate_data_file(data_file):
+        """
+        Generate data file for scheduling problem.
+        """
         # Get Supervisor Availability Data
         availability = Data.get_availability_data()
 
@@ -53,38 +58,36 @@ class Data:
         data = {
             "students": NUM_STUDENTS,
             "supervisors": NUM_SUPERVISORS,
-
             "timeslots": TIMESLOTS,
-            "sessions": SESSIONS,       
-
+            "sessions": SESSIONS,
             "max_students_per_session": MAX_STUDENTS_PER_SESSION,
             "max_students_per_supervisor": MAX_STUDENTS_PER_SUPERVISOR,
             "max_supervisors_per_session": MAX_SUPERVISORS_PER_SESSION,
             "min_supervisors_per_session": MIN_SUPERVISORS_PER_SESSION,
-
             "availability": availability,
         }
 
         # Write to .dzn file
-        with open(dataFile, 'w') as outfile:
+        with open(data_file, 'w') as outfile:
             outfile.write("\n".join(dzn.dict2dzn(data)))
 
-
-
+    @staticmethod
     def verify_output(result):
+        """
+        Verify that all students are assigned to a session.
+        """
         students = result['session_students']
 
         # Add up all students in all sessions
-        numStudents = 0
+        num_students = 0
         for timeslot in students:
             for session in timeslot:
                 for student in session:
-                    numStudents += 1
+                    num_students += 1
 
         # Verify all students assigned a slot
-        print("Num Students: ", numStudents)
-        if(numStudents != NUM_STUDENTS):
-                print("Invalid Output: Students missing from assignment")
+        if num_students != NUM_STUDENTS :
+                print(f"Invalid Output: {NUM_STUDENTS - num_students} students missing from assignment")
                 return False
         return True
 
--- FYP_Scheduling_Problem.py
+++ FYP_Scheduling_Problem.py
@@ -7,43 +7,72 @@ DATA_FILE = './data/fyp.dzn'
 SOLVER = 'gecode'
 NUM_RUNS = 1
 
-class FYP():
-    def main():
-        total_time = 0
-        succesful_runs = 0
-        
-        for x in range(NUM_RUNS):
-            
-            ## BUILD MODEL ##
-            fyp = Model(MODEL_FILE)
+def main():
+    """
+    Runs the MiniZinc instance NUM_RUNS times and prints the average runtime.
+    """
+    total_time = 0
+    successful_runs = 0
+    
+    for _ in range(NUM_RUNS):
+        model = build_model()
+        result, runtime = solve_instance(model)
+        if verify_output(result):
+            print_result(result, runtime)
+            total_time += runtime
+            successful_runs += 1
+    
+    if successful_runs > 0:
+        if NUM_RUNS == 1:
+            print("Single run completed.")
+        else:
+            print(f"Avg Time: {round(total_time / successful_runs, 3)}s")
+    else:
+        print("No successful runs.")
 
-            # Add data file to models
-            Data.generate_data_file(DATA_FILE)
-            fyp.add_file(DATA_FILE)
 
-            
-            ## SOLVE INSTANCE ##
-            gecode = Solver.lookup(SOLVER)
-            instance = Instance(gecode, fyp)
+def build_model():
+    """
+    Builds the MiniZinc model and adds the data file to it.
+    """
+    fyp = Model(MODEL_FILE)
+    Data.generate_data_file(DATA_FILE)
+    fyp.add_file(DATA_FILE)
+    return fyp
 
-            # Solve instance and record runtime
-            start_time = time.time()
-            result = instance.solve()
-            runtime = round(time.time() - start_time, 3)
 
-            ## VERIFY OUTPUT ##
-            if(Data.verify_output(result)):
-                print(result)
-                print(f"Time: {runtime}s")
-                total_time += runtime
-                succesful_runs += 1
-        
-        #print(f"Avg Time: {round(total_time / succesful_runs, 3)}s")
-        #print(result)
-            
+def solve_instance(model):
+    """
+    Solves the MiniZinc instance given the model.
+    """
+    gecode = Solver.lookup(SOLVER)
+    instance = Instance(gecode, model)
 
-    if __name__ == "__main__":
-        main()
+    start_time = time.time()
+    result = instance.solve()
+    runtime = round(time.time() - start_time, 3)
+
+    return result, runtime
+
+
+def verify_output(result):
+    """
+    Verifies that the output of the MiniZinc instance is correct.
+    """
+    return Data.verify_output(result)
+
+
+def print_result(result, runtime):
+    """
+    Prints the solution and runtime.
+    """
+    if NUM_RUNS == 1:
+        print(result)
+    print(f"Time: {runtime}s")          
+
+
+if __name__ == "__main__":
+    main()
 
     
     
